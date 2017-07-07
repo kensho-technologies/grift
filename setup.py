9c24d1516bae039a36ce02ac9709eda700d13111
Carolyn Ranti <carolyn@kensho.com>
2017-07-07 15:57:19 -0400

    v0.5.1 version bump
    Bug fixes:
    - Fix validation of each member in a ListType
    - Fix _disconnecting context manager for RedisType
    
    New features:
    - Added loaders to read from HashiCorp Vault

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'grift'
 
 setup(name=package_name,
-      version='0.5',
+      version='0.5.1',
       packages=find_packages(),
       package_data={package_name + '.tests': ['*.json']},
       install_requires=[
