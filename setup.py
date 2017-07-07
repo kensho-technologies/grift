efe28040baf4e5edfa636b2ff40276e9c6510c81
Carolyn Ranti <carolyn@kensho.com>
2017-07-07 16:44:07 -0400

    v0.5.2 add metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import find_packages, setup
 package_name = 'grift'
 
 setup(name=package_name,
-      version='0.5.1',
+      version='0.5.2',
+      description='A clean approach to app configuration',
+      keywords='app config configuration schema python',
+      maintainer_email='grift-maintainer@kensho.com',
+      url='https://github.com/kensho-technologies/grift',
       packages=find_packages(),
       package_data={package_name + '.tests': ['*.json']},
       install_requires=[
