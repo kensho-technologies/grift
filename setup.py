1ea092b4cc81929742f7bce90df373e878cde8bb
vsivaraja <vikram.sivaraja@kensho.com>
2017-10-03 15:27:16 -0400

    Upgrade version to 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'grift'
 
 setup(name=package_name,
-      version='0.5.1',
+      version='0.6.0',
       packages=find_packages(),
       package_data={package_name + '.tests': ['*.json']},
       install_requires=[
