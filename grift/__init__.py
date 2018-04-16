ba30a71bbbe131fd71fe68fd275f6470be2f107f
Predrag Gruevski <predrag@kensho.com>
2018-04-16 14:15:51 -0400

    Change "Kensho Technologies, Inc." to "Kensho Technologies, LLC."

--- grift/__init__.py
+++ grift/__init__.py
@@ -1,3 +1,3 @@
-# Copyright 2017 Kensho Technologies, Inc.
+# Copyright 2017 Kensho Technologies, LLC.
 from .config import BaseConfig, ConfigProperty  # noqa
 from .loaders import DictLoader, EnvLoader, JsonFileLoader  # noqa
--- grift/loaders.py
+++ grift/loaders.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Kensho Technologies, Inc.
+# Copyright 2017 Kensho Technologies, LLC.
 from abc import ABCMeta, abstractmethod
 import json
 import os
