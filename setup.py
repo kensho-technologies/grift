33fae063ef005983bd598b98dc782f9694f3e1bd
AlexPadron <alex@kensho.com>
2018-07-10 17:53:14 +0000

    updating requests restriction

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'grift'
 
 setup(name=package_name,
-      version='0.6.0',
+      version='0.7.0',
       description='A clean approach to app configuration',
       keywords='app config configuration schema python',
       maintainer_email='grift-maintainer@kensho.com',
@@ -12,7 +12,7 @@ setup(name=package_name,
       package_data={package_name + '.tests': ['*.json']},
       install_requires=[
           'schematics==1.1.1',
-          'requests==2.13.0',
+          'requests>=2.13.0',
           'six==1.11.0',
       ],
       )
